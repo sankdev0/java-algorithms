1e69e61e153aec909c377657187b110238e3e399
san k <sankdev0@gmail.com>
2021-11-08 20:46:50 +0500

    Remove IDE config directory
    Add Binary Search Tree implementation (NOT tested).

--- src/main/java/com/sankdev/datastructures/BSTImpl.java
+++ src/main/java/com/sankdev/datastructures/BSTImpl.java
@@ -115,6 +115,54 @@ class BSTImpl {
     // val is less than root's key
     return search_Recursive(root.right, key);
   }
+
+  //PostOrder Traversal - Left:Right:rootNode (LRn)
+  void postOrder(Node node)  {
+    if (node == null)
+      return;
+
+    // first traverse left subtree recursively
+    postOrder(node.left);
+
+    // then traverse right subtree recursively
+    postOrder(node.right);
+
+    // now process root node
+    System.out.print(node.key + " ");
+  }
+  // InOrder Traversal - Left:rootNode:Right (LnR)
+  void inOrder(Node node)  {
+    if (node == null)
+      return;
+    //first traverse left subtree recursively
+    inOrder(node.left);
+
+    //then go for root node
+    System.out.print(node.key + " ");
+
+    //next traverse right subtree recursively
+    inOrder(node.right);
+  }
+
+  //PreOrder Traversal - rootNode:Left:Right (nLR)
+  void preOrder(Node node)  {
+    if (node == null)
+      return;
+
+    //first print root node first
+    System.out.print(node.key + " ");
+    // then traverse left subtree recursively
+    preOrder(node.left);
+    // next traverse right subtree recursively
+    preOrder(node.right);
+  }
+  // Wrappers for recursive functions
+  void postOrder_traversal()  {
+    postOrder(root);  }
+  void inOrder_traversal() {
+    inOrder(root);   }
+  void preOrder_traversal() {
+    preOrder(root);  }
 }
 class Main{
   public static void main(String[] args)  {
@@ -155,5 +203,15 @@ class Main{
     System.out.println("\nKey 50 found in BST:" + ret_val );
     ret_val = bst.search (12);
     System.out.println("\nKey 12 found in BST:" + ret_val );
+    System.out.println();
+    //PreOrder Traversal
+    System.out.println("BST => PreOrder Traversal:");
+    bst.preOrder_traversal();
+    //InOrder Traversal
+    System.out.println("\nBST => InOrder Traversal:");
+    bst.inOrder_traversal();
+    //PostOrder Traversal
+    System.out.println("\nBST => PostOrder Traversal:");
+    bst.postOrder_traversal();
   }
 }
