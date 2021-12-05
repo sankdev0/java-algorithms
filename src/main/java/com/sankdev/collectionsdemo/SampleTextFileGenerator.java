4ec1de97a6d8ae5e5a33d137d6fc886ae3c7a215
san k <sankdev0@gmail.com>
2021-12-06 00:00:18 +0500

    Improve SampleTextFileGenerator to generate test file
    Generate a test text file that contains blank lines and duplicate lines

--- src/main/java/com/sankdev/collectionsdemo/SampleTextFileGenerator.java
+++ src/main/java/com/sankdev/collectionsdemo/SampleTextFileGenerator.java
@@ -5,22 +5,45 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Class is used to generate a large text file for testing purposes.
+ * Class is used to generate a sample text files for testing purposes.
  */
 public class SampleTextFileGenerator {
 
-  // number of lines in the target text file
-  private final int LINES = 1_000_000;
-
-  public void createSampleTextFile(String fileName) throws IOException {
+  /**
+   * Generates a text file with fileName containing lineCount lines. Each 25th line is blank (i.e.
+   * string of length 0). Each 15th and 101st line is duplicated, that means that blank lines will
+   * be duplicated. Lines are numbered to review the result, except for the blank lines.
+   *
+   * @param fileName  - target file name
+   * @param lineCount - number of lines to write to the file
+   * @throws IOException when cannot write to file
+   */
+  public void createSampleTextFile(String fileName, int lineCount) throws IOException {
     BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
 
-    for (int i = 0; i < LINES; i++) {
-      String stubLine = " This is a test line for a test file. Makes no sense. \n";
-      writer.write(i + stubLine);
+    String prevStubLine = "";
+    for (int i = 0; i < lineCount; i++) {
+      String stubLine;
+
+      if (i != 0 && i % 25 == 0) {
+        stubLine = "\n";
+      } else if (i != 0 && (i % 15 == 0 || i % 101 == 0)) {
+        stubLine = prevStubLine;
+      } else {
+        stubLine = i + ". This is a test line for a test file. Makes no sense. \n";
+      }
+
+      writer.write(stubLine);
+
+      prevStubLine = stubLine;
     }
 
     writer.close();
   }
 
+  public static void main(String[] args) throws IOException {
+    SampleTextFileGenerator generator = new SampleTextFileGenerator();
+    generator.createSampleTextFile(
+        System.getProperty("java.io.tmpdir") + "test-file.txt", 2_000);
+  }
 }
