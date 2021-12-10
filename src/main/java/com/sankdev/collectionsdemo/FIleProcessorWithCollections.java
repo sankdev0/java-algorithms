82444c20d186664f22d852ed850ff9d61d51fc9a
san k <sankdev0@gmail.com>
2021-12-10 21:44:31 +0500

    Add FIleProcessorWithCollections:
    It is a demo that shows how to use Java Collections Framework to process a text file line by line

--- src/main/java/com/sankdev/collectionsdemo/FIleProcessorWithCollections.java
+++ src/main/java/com/sankdev/collectionsdemo/FIleProcessorWithCollections.java
@@ -1,48 +1,51 @@
 package com.sankdev.collectionsdemo;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
+ * This class shows use cases of Java Collections Framework for processing a text file line by line.
  * Solution to Exercise 1.1. in the book "Open Data Structures" by Pat Morin. The idea is to choose
  * the right data structure for the right problem. If implemented, the parts of this exercise should
  * be done by making use of an implementation of the relevant interface (Stack, Queue, Deque, USet,
  * or SSet) provided by the Java Collections Framework.
  */
-public class CollectionsUseCases {
+public class FIleProcessorWithCollections {
 
   /**
    * Read the input one line at a time and then write the lines out in reverse order, so that the
    * last input line is printed first, then the second last input line, and so on.
    *
-   * @param input
-   * @param outputFileName
-   * @return
-   * @throws IOException
+   * @param input          path to the input file
+   * @param outputFileName output file name
+   * @return path to the output file
+   * @throws IOException when IO operations fail
    */
   public Path reverseLines(Path input, String outputFileName) throws IOException {
 
-    Stack<String> lineStack = new Stack<>();
+    Deque<String> lineStack = new ArrayDeque<>();
 
-    Path tmpPath = Paths.get(System.getProperty("java.io.tmpdir"));
-    Path output = tmpPath.resolve(outputFileName);
-
-    // Push into the stack.
-    try(BufferedReader br = Files.newBufferedReader(input)) {
+    // 1. Read the input file line by line and push lines into the stack.
+    try (BufferedReader br = Files.newBufferedReader(input)) {
       String line;
       while ((line = br.readLine()) != null) {
         lineStack.push(line);
       }
     }
 
-    // Pop from the stack.
+    Path output = Files.createTempFile(outputFileName, ".txt");
 
-
+    // 2. Pop from the stack and write to the output file.
+    try (BufferedWriter bw = Files.newBufferedWriter(output)) {
+      while (!lineStack.isEmpty()) {
+        bw.write(lineStack.pop());
+      }
+    }
 
     return output;
   }
